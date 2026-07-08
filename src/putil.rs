a2ccf373e57e9ceade6b41e2f5fa4344bb758faf
Ian Chang <ianchang2007@icloud.com>
2026-07-08 13:35:45 -0400

    changed pluralizer to accept es and pluralize capital letters if word is capitalized

--- src/putil.rs
+++ src/putil.rs
@@ -58,7 +58,22 @@ pub fn return_literal(
     Ok(value.clone())
 }
 
-// Could be more sophisticated.
+// Pluralize a string by adding an "s" or "es" to the end. Does not handle complex pluralization rules, but handles the most common cases.
 pub fn pluralize(s: &str) -> String {
-    format!("{}s", s)
+    let ls = s.to_lowercase();
+    let possible_es = ["s", "sh", "ch", "x", "z"];
+    let consonants = "bcdfghjklmnpqrstvwxyz";
+    let word_with_consonant_o = ls.ends_with("o")
+        && ls
+            .chars()
+            .rev()
+            .nth(1)
+            .is_some_and(|c| consonants.contains(c));
+    let needs_es = word_with_consonant_o || possible_es.iter().any(|&ending| ls.ends_with(ending));
+    let suffix = if needs_es { "es" } else { "s" };
+    if s.chars().last().is_some_and(char::is_uppercase) {
+        format!("{s}{}", suffix.to_uppercase())
+    } else {
+        format!("{s}{suffix}")
+    }
 }
