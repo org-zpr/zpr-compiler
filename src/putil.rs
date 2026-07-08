9544357a497ce865ad58df0863ee2531bd0ca60d
ianchang1 <131323440+ianchang1@users.noreply.github.com>
2026-07-08 15:24:01 -0400

    changed pluralizer to accept es and pluralize capital letters if word… (#123)
    I changed the pluralize function to handle es cases instead of simply
    just adding s. It also adds capital s or es if the word ends with a
    capital letter. While I was looking into pluralization rules I found
    that rust has a pluralize package that handles abstract cases like mouse
    -> mice which I can also implement since there is a lot of random
    pluralization rules.

--- src/putil.rs
+++ src/putil.rs
@@ -58,7 +58,38 @@ pub fn return_literal(
     Ok(value.clone())
 }
 
-// Could be more sophisticated.
+// Pluralize a string by adding an "s" or "es" to the end. Does not handle complex pluralization rules, but handles the most common cases.
 pub fn pluralize(s: &str) -> String {
-    format!("{}s", s)
+    let ls = s.to_lowercase();
+    let possible_es = ["s", "sh", "ch", "x", "z"];
+    let consonants = "bcdfghjklmnpqrstvwxyz";
+    let word_with_consonant_o = ls.ends_with("o")
+        && ls
+            .chars()
+            .rev()
+            .nth(1)
+            .is_some_and(|c| consonants.contains(c));
+    let needs_es = word_with_consonant_o || possible_es.iter().any(|&ending| ls.ends_with(ending));
+    let suffix = if needs_es { "es" } else { "s" };
+    if s.chars().last().is_some_and(char::is_uppercase) {
+        format!("{s}{}", suffix.to_uppercase())
+    } else {
+        format!("{s}{suffix}")
+    }
+}
+
+#[test]
+fn test_pluralize() {
+    assert_eq!(pluralize("mobile-phone"), "mobile-phones");
+    assert_eq!(pluralize("box"), "boxes");
+    assert_eq!(pluralize("bus"), "buses");
+    assert_eq!(pluralize("match"), "matches");
+    assert_eq!(pluralize("dish"), "dishes");
+    assert_eq!(pluralize("potato"), "potatoes");
+    assert_eq!(pluralize("radio"), "radios");
+    assert_eq!(pluralize("VisaService"), "VisaServices");
+    assert_eq!(pluralize("BOX"), "BOXES");
+    assert_eq!(pluralize("POTATO"), "POTATOES");
+    assert_eq!(pluralize("o"), "os");
+    assert_eq!(pluralize(""), "s");
 }
